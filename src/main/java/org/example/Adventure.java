6c14bb6698d94193510f44813a72a9f740619511
milasKEA <miso0002@stud.kea.dk>
2023-09-28 10:49:29 +0200

    //

--- src/main/java/org/example/Adventure.java
+++ src/main/java/org/example/Adventure.java
@@ -12,4 +12,42 @@ public class Adventure {
         map.buildWorld();
         player.setCurrentPosition(map.getStartingRoom());
     }
+
+    public Room getStartingRoom() {
+        return map.getStartingRoom();
+    }
+
+    public Room getPlayerPosition() {
+        return player.getPlayerPosition();
+    }
+
+    public boolean movePlayer(String direction) {
+        switch (direction) {
+            case "north":
+                if (player.getPlayerPosition().getNorth() != null) {
+                    player.setCurrentPosition(player.getPlayerPosition().getNorth());
+                    return true;
+                } else return false;
+
+            case "south":
+                if (player.getPlayerPosition().getSouth() != null) {
+                    player.setCurrentPosition(player.getPlayerPosition().getSouth());
+                    return true;
+                } else return false;
+
+            case "east":
+                if (player.getPlayerPosition().getEast() != null) {
+                    player.setCurrentPosition(player.getPlayerPosition().getEast());
+                    return true;
+                } else return false;
+
+            case "west":
+                if (player.getPlayerPosition().getWest() != null) {
+                    player.setCurrentPosition(player.getPlayerPosition().getWest());
+                    return true;
+                } else return false;
+        }
+        return false;
+    }
 }
+
