ef8624a22f1b2ecddbdaaf2982cef14271deacd1
milasKEA <miso0002@stud.kea.dk>
2023-09-28 10:48:57 +0200

    Created Player class to SRP movement

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Player {
+    private Room currentPosition;
+    //inventory
+
+    public Player() {
+    }
+
+
+    public void setCurrentPosition(Room position) {
+        currentPosition = position;
+    }
+
+    public Room getPlayerPosition(){
+        return currentPosition;
+    }
+
+}
+
