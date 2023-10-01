a025f5de6068aa1838f5cd6a70aedc974ff11045
Lars <laha0006@stud.kea.dk>
2023-10-02 00:08:49 +0200

    removed explicit call to game.start. Now that start is called implicitly, when you make a new Adventure object.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Adventure game = new Adventure();
-        game.start();
         UserInterface ui = new UserInterface(game);
         ui.start();
     }
