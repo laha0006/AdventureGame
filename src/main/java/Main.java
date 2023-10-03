7a0c123896485ba65a54c8d327c601f1ddffc129
Lars <laha0006@stud.kea.dk>
2023-10-03 14:48:31 +0200

    added direction enum.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,6 @@
-import game.Adventure;
-import ui.UserInterface;
-
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.IOException;
-
-
-public class Main {
-    public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+public class
+Main {
+    public static void main(String[] args) {
         Adventure game = new Adventure();
         UserInterface ui = new UserInterface(game);
         ui.start();
