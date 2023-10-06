88db23685da781827e95e09c43366cab84706d34
Lars <laha0006@stud.kea.dk>
2023-10-06 18:31:02 +0200

    updated main to reflect structure change.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
-public class Main {
+import game.Adventure;
+import ui.UserInterface;
+
+public class
+Main {
     public static void main(String[] args) {
         Adventure game = new Adventure();
         UserInterface ui = new UserInterface(game);
