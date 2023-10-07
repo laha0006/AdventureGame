1d7cd1911334e54602f8dd005de32f62c8b3c315
Lars <laha0006@stud.kea.dk>
2023-10-07 02:59:36 +0200

    added imports, and throws exceptions declarations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import game.Adventure;
 import ui.UserInterface;
 
-public class
-Main {
-    public static void main(String[] args) {
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
         Adventure game = new Adventure();
         UserInterface ui = new UserInterface(game);
         ui.start();
