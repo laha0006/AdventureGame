febc019ce24c58068126d0a3c0d5adfb92f2cc46
Lars <laha0006@stud.kea.dk>
2023-10-07 21:23:07 +0200

    updated MeleeWeapon with an overloaded constructor that can take in Audio attackSound

--- src/main/java/item/MeleeWeapon.java
+++ src/main/java/item/MeleeWeapon.java
@@ -1,9 +1,16 @@
 package item;
 
+import game.Audio;
+
 public class MeleeWeapon extends Weapon {
     private int attackPower;
+
+
     public MeleeWeapon(String longName, String shortName, int damage, int durability, int attackPower) {
         super(longName, shortName, damage, durability);
+    }
+    public MeleeWeapon(String longName, String shortName, int damage, int durability, int attackPower, Audio attackSound) {
+        super(longName, shortName, damage, durability,attackSound);
     }
 
     @Override
