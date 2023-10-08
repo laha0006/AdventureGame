783f4712a611fbc6155436b9960108a10e8f4536
Lars <laha0006@stud.kea.dk>
2023-10-08 23:59:28 +0200

    made clip load in IDE and from Jar.

--- src/main/java/game/Audio.java
+++ src/main/java/game/Audio.java
@@ -2,16 +2,32 @@ package game;
 
 
 import javax.sound.sampled.*;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 
 public class Audio {
     Clip audioClip;
     public Audio(String audioFilePath) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(audioFilePath).getAbsoluteFile());
-        audioClip = AudioSystem.getClip();
-        audioClip.open(audioStream);
+
+        InputStream inputStream = getClass().getResourceAsStream("/sounds/"+audioFilePath);
+        if (inputStream != null) { // if inputStream is not null, means jar file, and hence used getResource as stream.
+            InputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedInputStream);
+            audioClip = AudioSystem.getClip();
+            audioClip.open(audioStream);
+        } else { // if inputStream is null, means we're not in a jar, and need absolute file path.
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File("sounds/"+audioFilePath).getAbsoluteFile());
+            audioClip = AudioSystem.getClip();
+            audioClip.open(audioStream);
+        }
+
+
+
+
+
         FloatControl volumeControl = (FloatControl) audioClip.getControl(FloatControl.Type.MASTER_GAIN);
         volumeControl.setValue(-40f);
     }
@@ -26,9 +42,12 @@ public class Audio {
     }
 
     public void playOnce() {
-        if (audioClip.isActive()) audioClip.stop();
-        audioClip.setMicrosecondPosition(0);
+        if (audioClip.isActive()) {
+            audioClip.stop();
+        }
+        audioClip.setMicrosecondPosition(1);
         audioClip.start();
+        System.out.println("Play sound");
     }
 
 
