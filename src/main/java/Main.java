6d10e5c167c9839f78af3da75b4700f3b02bd427
Lars <laha0006@stud.kea.dk>
2023-10-09 22:18:08 +0200

    refactored Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
-public class
-Main {
+import game.Adventure;
+import ui.UserInterface;
+public class Main {
     public static void main(String[] args) {
         Adventure game = new Adventure();
         UserInterface ui = new UserInterface(game);
