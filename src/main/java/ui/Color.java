d078ad1055f87f522fb0e71f514e894a2e94a960
Lars <laha0006@stud.kea.dk>
2023-10-10 20:21:26 +0200

    added ORANGE color.

--- src/main/java/ui/Color.java
+++ src/main/java/ui/Color.java
@@ -13,6 +13,7 @@ public class Color {
     public static final String PURPLE = "\033[0;35m";  // PURPLE
     public static final String CYAN = "\033[0;36m";    // CYAN
     public static final String WHITE = "\033[0;37m";   // WHITE
+    public static final String ORANGE = "\033[38;5;208m"; // ORANGE
 
     // Bold
     public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
