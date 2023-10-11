36810819dcded6806221b6905e21f94048bf6318
Lars <laha0006@stud.kea.dk>
2023-10-11 02:27:36 +0200

    added colors for items.

--- src/main/java/ui/Color.java
+++ src/main/java/ui/Color.java
@@ -9,11 +9,14 @@ public class Color {
     public static final String RED = "\033[0;31m";     // RED
     public static final String GREEN = "\033[0;32m";   // GREEN
     public static final String YELLOW = "\033[0;33m";  // YELLOW
-    public static final String BLUE = "\033[0;34m";    // BLUE
+    public static final String BLUE = "\033[0;105m";    // BLUE
     public static final String PURPLE = "\033[0;35m";  // PURPLE
     public static final String CYAN = "\033[0;36m";    // CYAN
-    public static final String WHITE = "\033[0;37m";   // WHITE
+    public static final String WHITE = "\033[0;255m";   // WHITE
     public static final String ORANGE = "\033[38;5;208m"; // ORANGE
+    public static final String PURPLE_EPIC = "\033[38;5;163m"; // WOW PURPLE
+    public static final String GREEN_UNCOMMON = "\033[38;5;30m"; // WOW GREEN
+    public static final String BLUE_RARE = "\033[38;5;63m"; // WoW Rare
 
     // Bold
     public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
